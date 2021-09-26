cc18d16f92d54d2e5b5dcbec64ff899dc15cb552
phoenix34 <phoenix34@protonmail.com>
2021-09-27 01:38:44 +0200

    no dependencies for datajson converter

--- src/converter/DataJsonConverter.ts
+++ src/converter/DataJsonConverter.ts
@@ -4,27 +4,33 @@ import {
   OpenTimeOutput,
 } from "../OpeningHours";
 import { Converter } from "../Converter";
-import { DisplayJsonConverter } from "./DisplayJsonConverter";
 
 export class DataJsonConverter implements Converter<OpenTimeOutput[]> {
   convert(openingHours: OpeningHours, options: OpeningHoursOptions = {}) {
-    const result: OpenTimeOutput[] = [];
+    options = { ...openingHours.options, ...options };
     const format: Intl.DateTimeFormatOptions = {
-      ...openingHours.options.dateTimeFormatOptions,
+      ...options.dateTimeFormatOptions,
     };
     delete format.timeZone;
 
-    const { weekDays } = { ...openingHours.text, ...(options.text || {}) };
-    const jsonConverter = new DisplayJsonConverter();
-    const openTimes = jsonConverter.convert(openingHours, options);
+    const { locales } = options;
+    const result: OpenTimeOutput[] = [];
 
-    for (const times of openTimes) {
-      for (const time of times.times) {
-        result.push({
-          day: weekDays.indexOf(times.day),
-          from: time.from.replace(/:/g, ""),
-          until: time.until.replace(/:/g, ""),
-        });
+    for (const [day, times] of openingHours.times.entries()) {
+      if (times.length !== 0) {
+        // insert opening hours with the correct time format and
+        // add the translation of the current day.
+        result.push(
+          ...times.map((time) => {
+            const from = time.from
+              .toLocaleTimeString(locales, format)
+              .replace(/:/g, "");
+            const until = time.until
+              .toLocaleTimeString(locales, format)
+              .replace(/:/g, "");
+            return { day, from, until };
+          })
+        );
       }
     }
     return result;
