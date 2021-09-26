992e984a572a4cd7798d2c4cb0f31a708fc60e1a
phoenix34 <phoenix34@protonmail.com>
2021-09-27 01:30:16 +0200

    remove unused simple format

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,3 @@
-/**
- * Create a simplified 24h time format without any separator.
- */
-export function convertToSimpleFormat(date: Date, timeZone?: string) {
-  const [hours, minutes, seconds] = date
-    .toLocaleTimeString("sv", { timeZone })
-    .split(":");
-  return (
-    ("00" + hours).slice(-2) +
-    ("00" + minutes).slice(-2) +
-    (seconds !== "00" ? ("00" + seconds).slice(-2) : "")
-  );
-}
-
 export function normalizeLocalDate(date: Date, timeZone?: string | undefined) {
   // Hack: I'm using the "sv" locale from sweden,
   // because it's similar to the ISO DateTime format.
